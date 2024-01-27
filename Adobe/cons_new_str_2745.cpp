bd10fa7c7237bf926bdb316a69898e92e0d2b755
roushanku <roushankuiitbhilai.ac.in>
2024-01-27 11:11:18 +0530

    DP will give TLE -> try greedy simple math fromula will came based on case of x and y

---
+++ Adobe/cons_new_str_2745.cpp
@@ -0,0 +1,40 @@
+// J.S.R -- *roush*
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1000000007;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds; //find_by_order //order_of_key
+class Solution {
+public:
+    int longestString(int x, int y, int z) {
+        int ans = 0;
+
+        if(x < y) {
+            ans = ((2*x + 1) * 2 + 2*z);
+        }
+
+        else if(x > y){
+            ans = ((2*y + 1) * 2 + 2*z);
+        }
+
+        else ans = (x + y + z) * 2;
+
+        return ans;
+    }
+};
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+//patience Bro , thoda sa logic soch k try ur own test case
+//hadbad m kaafi gadbad kie ho islie aaram s -> You will do best for Youself
+//First, solve the problem. Then, write the code.
+int t;
+cin>>t;
+while(t--){
+           
+}
+return 0;
+}
